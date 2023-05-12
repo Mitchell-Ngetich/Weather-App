a5bfae1fe2e25781ba4a31bf4a46b82e3997ad03
Mitchell Ngetich <mitchell.ngetich@student.moringaschool.com>
2023-05-12 12:16:26 +0300

    Created app components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import Sunny from './Assets/Sunny.jpg';
+import Cold from './Assets/cold.jpg';
+import Description from './Components/Description/Description';
+import { useEffect, useState } from 'react';
+import { getWeatherData } from './Components/Weather/Weather';
+import { FaCity } from 'react-icons/fa';
+
 
 function App() {
+	const [city, setCity] = useState("Paris")
+	const [weather, setWeather] = useState(null);
+	const [units, setUnits] = useState("metric");
+
+	useEffect(() => {
+		const fetchWeatherData = async () => {
+			const data = await getWeatherData(city, units);
+			setWeather(data);
+		};
+		fetchWeatherData();
+		
+	}, [units, city])
+
+	const handleUnitsClick = (e) => {
+		const button = e.currentTarget;
+		const currentUnit = button.innerText.slice(1);
+		const isCelsius = currentUnit === "C";
+		button.innerText = isCelsius ? "°F" : "°C";
+		setUnits(isCelsius? "metric" : "imperial")
+	};
+
+	const enterKeyPressed = (e) => {
+		if (e.keyCode === 13) {
+			setCity(e.currentTarget.value);
+			e.currentTarget.blur();
+		}
+	}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="app" style={{ backgroundImage: `url(${Cold})` }}>
+			<div className="overlay">
+				{weather && (
+					<div className="container">
+						<div className="section section__inputs">
+							<input onKeyDown={enterKeyPressed}
+								type="text"
+								name="city"
+								placeholder="Enter City..."></input>
+							<button onClick={(e) => handleUnitsClick(e)}>°F</button>
+						</div>
+
+						<div className="section section__temperature">
+							<div className="icon">
+								<h3>{`${weather.name}, ${weather.country}`}</h3>
+								<img
+									src={weather.iconURL}
+									alt="weatherIcon"
+									className="image"
+								/>
+								<h3>{weather.description}</h3>
+							</div>
+							<div className="temperature">
+								<h1>{`${weather.temp.toFixed()} °${
+									units === "metric" ? "C" : "F"
+								}`}</h1>
+							</div>
+						</div>
+
+						{/* Descriptions */}
+						<Description weather={weather} units={units} />
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
